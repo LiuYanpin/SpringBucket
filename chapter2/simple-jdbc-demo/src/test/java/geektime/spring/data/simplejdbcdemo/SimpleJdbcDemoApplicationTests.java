c26b9d6de9a560fbbf7e7f0a66e01c9cc3a495ae
Yanping <yanpliu@thoughtworks.com>
2020-05-27 19:45:04 +0800

    [Yanping] add Simple Jdbc Demo

---
+++ chapter2/simple-jdbc-demo/src/test/java/geektime/spring/data/simplejdbcdemo/SimpleJdbcDemoApplicationTests.java
@@ -0,0 +1,19 @@
+package geektime.spring.data.simplejdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class SimpleJdbcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void should_be_true() {
+        assertTrue(true);
+    }
+}
