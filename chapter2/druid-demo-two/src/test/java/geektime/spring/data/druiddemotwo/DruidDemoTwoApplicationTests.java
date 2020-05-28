6ed092b63b3c422ec50716725c4f9d486a5a5b99
Yanping <yanpliu@thoughtworks.com>
2020-05-28 22:26:07 +0800

    [Yanping]add druid demo

---
+++ chapter2/druid-demo-two/src/test/java/geektime/spring/data/druiddemotwo/DruidDemoTwoApplicationTests.java
@@ -0,0 +1,19 @@
+package geektime.spring.data.druiddemotwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class DruidDemoTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void should_be_true() {
+        assertTrue(true);
+    }
+}
