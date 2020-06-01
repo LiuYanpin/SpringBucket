9f0a7a03a4a4b941b61565b070fe509002d4fe71
Yanping <yanpliu@thoughtworks.com>
2020-06-01 22:47:13 +0800

    [Yanping] add redis demo

--- chapter4/redis-demo/src/main/java/geektime/spring/springbucks/SpringbucksApplication.java
+++ chapter4/redis-demo/src/main/java/geektime/spring/springbucks/SpringbucksApplication.java
@@ -1,21 +1,27 @@
 package geektime.spring.springbucks;
 
+import geektime.spring.springbucks.model.Coffee;
 import geektime.spring.springbucks.service.CoffeeService;
+import io.lettuce.core.ReadFrom;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Optional;
 
 @Slf4j
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaRepositories
-@EnableCaching(proxyTargetClass = true)
 public class SpringbucksApplication implements ApplicationRunner {
 	@Autowired
 	private CoffeeService coffeeService;
@@ -24,15 +30,25 @@ public class SpringbucksApplication implements ApplicationRunner {
 		SpringApplication.run(SpringbucksApplication.class, args);
 	}
 
+	@Bean
+	public RedisTemplate<String, Coffee> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Coffee> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
+	}
+
+	@Bean
+	public LettuceClientConfigurationBuilderCustomizer customizer() {
+		return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.MASTER_PREFERRED);
+	}
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		log.info("Count: {}", coffeeService.findAllCoffee().size());
-		for (int i = 0; i < 10; i++) {
-			log.info("Reading from cache.");
-			coffeeService.findAllCoffee();
+		Optional<Coffee> mocha = coffeeService.findOneCoffee("mocha");
+		log.info("Coffee {}", mocha);
+		for (int i = 0; i < 5; i++) {
+			log.info("Value from Redis: {}", coffeeService.findOneCoffee("mocha"));
+
 		}
-		Thread.sleep(5000);
-		log.info("Reading after refresh");
-		coffeeService.findAllCoffee().forEach(c -> log.info("Coffee {}", c.getName()));
 	}
 }
