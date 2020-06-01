0ae27eb1d9b18611653a37750490f2fc37fe4e89
Yanping <yanpliu@thoughtworks.com>
2020-06-01 11:07:09 +0800

    [Yanping] add mybatis demo

---
+++ chapter3/mybatis-demo/src/main/java/geektime/spring/data/mybatisdemo/MybatisDemoApplication.java
@@ -0,0 +1,42 @@
+package geektime.spring.data.mybatisdemo;
+
+import geektime.spring.data.mybatisdemo.mapper.CoffeeMapper;
+import geektime.spring.data.mybatisdemo.model.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+@MapperScan("geektime.spring.data.mybatisdemo.mapper")
+public class MybatisDemoApplication implements ApplicationRunner {
+    @Autowired
+    private CoffeeMapper coffeeMapper;
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Coffee coffee = Coffee.builder().name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .build();
+        int count = coffeeMapper.save(coffee);
+        log.info("Save {} Coffee: {}", count, coffee);
+
+        Coffee coffee2 = Coffee.builder().name("latte")
+                .price(Money.of(CurrencyUnit.of("CNY"), 25.0))
+                .build();
+        int count2 = coffeeMapper.save(coffee2);
+        log.info("Save {} Coffee: {}", count2, coffee2);
+
+        log.info("Find Coffee: {}", coffeeMapper.findById(coffee.getId()));
+        log.info("Find Coffee: {}", coffeeMapper.findById(coffee2 .getId()));
+    }
+}
---
+++ chapter3/mybatis-demo/src/test/java/geektime/spring/data/mybatisdemo/MybatisDemoApplicationTests.java
@@ -0,0 +1,20 @@
+package geektime.spring.data.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class MybatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void should_be_true() {
+        assertTrue(true);
+    }
+
+}
